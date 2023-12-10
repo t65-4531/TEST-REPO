69753db3242e76050d98e510be3f3c53bf773259
t65-4531 <153447236+t65-4531@users.noreply.github.com>
2023-12-10 21:49:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  const resp = req.query;
+  res.send(`Hello World! ${resp}`);
 })
 
 app.listen(port, () => {
